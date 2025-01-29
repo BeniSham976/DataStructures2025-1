81db1ca2e632711426aef0b1105ca8c872deb149
michelle <michelle.graham@dkit.ie>
2025-01-29 09:58:29 +0000

    Testing: Tested indexOf() in IntDynamicArray

---
+++ src/test/java/utils/IntDynamicArrayTest.java
@@ -0,0 +1,31 @@
+package utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IntDynamicArrayTest {
+    @Test
+    public void testIndexOf_ValuePresent() {
+        IntDynamicArray myList = new IntDynamicArray();
+        myList.add(5);
+        myList.add(10);
+        myList.add(15);
+
+        int expResult = 1;
+        int result = myList.indexOf(10);
+        assertEquals(expResult, result, "Failed on locating valid value");
+    }
+
+    @Test
+    public void testIndexOf_ValueNotPresent(){
+        IntDynamicArray myList = new IntDynamicArray();
+        myList.add(5);
+        myList.add(10);
+        myList.add(15);
+
+        int expResult = -1;
+        int result = myList.indexOf(20);
+        assertEquals(expResult, result, "Incorrectly located invalid value");
+    }
+}
