7a970f9bfa60f2bb801440ee2d921a1eb4df38a8
michelle <michelle.graham@dkit.ie>
2025-02-18 23:40:20 +0000

    Feature: Add addFirst() and indexOf() functionality to LinkBasedList data structure
      - addFirst(): add a new element to the start of a LinkBasedList
      - indexOf(): search for the position of a specified element in a LinkBasedList

--- src/main/java/utils/LinkBasedList.java
+++ src/main/java/utils/LinkBasedList.java
@@ -62,6 +62,58 @@ public class LinkBasedList {
         numElements++;
     }
 
+
+    public void addFirst(String value){
+        // VALIDATION:
+        // Nulls are not permitted - throw exception if one is provided
+        if(value == null){
+            throw new IllegalArgumentException("List cannot handle null elements.");
+        }
+
+        // SETUP:
+        // Wrap value in a node so it can be added to the list
+        Node newNode = new Node(value);
+        // LOGIC:
+        // If list is currently empty, the new node should be the first and last element
+        if(isEmpty()){
+            first = newNode;
+            last = newNode;
+        }else{
+            // Put the new node in front of the current first element
+            newNode.next = first;
+            // Set the new node as the official start of the list
+            first = newNode;
+        }
+        // Increase the number of elements in the list
+        numElements++;
+    }
+
+
+    public int indexOf(String value){
+        // VALIDATION:
+        // Nulls are not permitted - throw exception if one is provided
+        if(value == null){
+            throw new IllegalArgumentException("List cannot handle null elements.");
+        }
+
+        // SETUP:
+        // Create a node to track progress in list and point it at the first thing in the list
+        // This will let us move through the list, accessing each node in order
+        Node current = first;
+        // Loop to the end of the list
+        for(int i = 0; i < numElements; i++){
+            // Check if the current node contains the data being looked for
+            if(value.equalsIgnoreCase(current.data)){
+                // If so, return this node's position in the list
+                return i;
+            }
+            // If it doesn't match, move our tracker node on to the next position in the list
+            current = current.next;
+        }
+        // No match found, return -1
+        return -1;
+    }
+
     private static class Node{
         private String data;
         private Node next;
