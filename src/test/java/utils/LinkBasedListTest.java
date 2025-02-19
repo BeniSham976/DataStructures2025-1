764a9b558b9808f1bac53e8807568c0eb75b6ba9
michelle <michelle.graham@dkit.ie>
2025-02-19 00:09:45 +0000

    Test: test get() of LinkBasedList
    Test cases covered:
        Invalid cases:
            - get from a negative position
            - get from after the end of the list (> length)
            - get from position equal to length of list (== length)
        Valid cases:
            - get from position 1 of a populated list (multiple elements in list)
            - get from position 0 (start position) of a populated list
            - get from end position of a populated list (multiple elements)

--- src/test/java/utils/LinkBasedListTest.java
+++ src/test/java/utils/LinkBasedListTest.java
@@ -91,4 +91,90 @@ class LinkBasedListTest {
         assertEquals(expSize, testList.size(), "Incorrect size after add to empty list");
         assertEquals(expValue, testList.get(0), "Value added to empty list could not be retrieved from position 0");
     }
+
+    /*
+     * Tests for get():
+     * Invalid cases:
+     *  - get from a negative position
+     *  - get from after the end of the list (> length)
+     *  - get from position equal to length of list (== length)
+     *
+     * Valid cases:
+     *  - get from position 1 of a populated list (multiple elements in list)
+     *  - get from position 0 (start position) of a populated list
+     *  - get from end position of a populated list (multiple elements)
+     */
+    @Test
+    public void testGet_NegativePosition() {
+        LinkBasedList myList = new LinkBasedList();
+        myList.add("Hi");
+        myList.add("Hello");
+        myList.add("Hey there");
+
+        assertThrows(IndexOutOfBoundsException.class, () -> myList.get(-1), "IndexOutOfBoundsException not thrown when attempting to get from negative position in list");
+    }
+
+    @Test
+    public void testGet_AboveBoundary_EqualToSize() {
+        LinkBasedList myList = new LinkBasedList();
+        myList.add("Hi");
+        myList.add("Hello");
+        myList.add("Hey there");
+
+        assertThrows(IndexOutOfBoundsException.class, () -> myList.get(3), "IndexOutOfBoundsException not thrown when attempting to get from position=length of list");
+    }
+
+    @Test
+    public void testGet_AboveBoundary_GreaterThanSize() {
+        // "Manual" list population
+        LinkBasedList myList = new LinkBasedList();
+        myList.add("Hi");
+        myList.add("Hello");
+        myList.add("Hey there");
+
+        assertThrows(IndexOutOfBoundsException.class, () -> myList.get(4), "IndexOutOfBoundsException not thrown when attempting to get from position > length of list");
+    }
+
+    @Test
+    public void testGet_ZeroIndex_PopulatedList() {
+        // "Automated" list population
+        String [] sourceData = {"Hi", "Hello", "Hey there"};
+        LinkBasedList myList = new LinkBasedList();
+        for(int i = 0; i < sourceData.length; i++){
+            myList.add(sourceData[i]);
+        }
+
+        int pos = 0;
+        String expResult = sourceData[pos];
+        String result = myList.get(pos);
+        assertEquals(expResult, result, "Value not found at position "+pos + ".");
+    }
+
+    @Test
+    public void testGet_LastElement_PopulatedList() {
+        String [] sourceData = {"Hi", "Hello", "Hey there"};
+        LinkBasedList myList = new LinkBasedList();
+        for(int i = 0; i < sourceData.length; i++){
+            myList.add(sourceData[i]);
+        }
+
+        int pos = sourceData.length-1;
+        String expResult = sourceData[pos];
+        String result = myList.get(pos);
+        assertEquals(expResult, result, "Value not found at position " + pos + " (last position in list).");
+    }
+
+    @Test
+    public void testGet_ElementInPopulatedList() {
+        String [] sourceData = {"Hi", "Hello", "Hey there"};
+        LinkBasedList myList = new LinkBasedList();
+        for(int i = 0; i < sourceData.length; i++){
+            myList.add(sourceData[i]);
+        }
+
+        int pos = sourceData.length/2;
+        String expResult = sourceData[pos];
+        String result = myList.get(pos);
+        assertEquals(expResult, result, "Value not found at position " + pos + " (position within bounds of list).");
+    }
 }
