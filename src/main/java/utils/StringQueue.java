4ad1ca07468670e3390f478793d8eb3cbc59fd2b
michelle <michelle.graham@dkit.ie>
2025-03-12 10:55:21 +0000

    Feature: Add Queue structure (containing Strings)
    Queue has been built as subclass of LinkBasedList.

---
+++ src/main/java/utils/StringQueue.java
@@ -0,0 +1,40 @@
+package utils;
+
+public class StringQueue extends LinkBasedList{
+    @Override
+    public void addFirst(String value) {
+        throw new UnsupportedOperationException("Cannot add to the start of Queue");
+    }
+
+    @Override
+    public void add(String value, int pos) {
+        throw new UnsupportedOperationException("Cannot add to a specified position of Queue");
+    }
+
+    @Override
+    public int indexOf(String value) {
+        throw new UnsupportedOperationException("Cannot search the contents of Queue");
+    }
+
+    @Override
+    public String remove(int pos) {
+        throw new UnsupportedOperationException("Cannot remove from specified position in Queue");
+    }
+
+    public String peek(){
+        if(isEmpty()){
+            throw new IllegalStateException("Cannot retrieve from an empty Queue");
+        }
+        return get(0);
+    }
+    
+    public String remove(){
+        if(isEmpty()){
+            throw new IllegalStateException("Cannot retrieve from an empty Queue");
+        }
+        
+        return remove(0);
+    }
+    
+    
+}
